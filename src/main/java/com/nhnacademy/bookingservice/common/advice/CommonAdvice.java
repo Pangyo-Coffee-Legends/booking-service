5fde4bd6becca4bcdd6ee461ce774a7fdbccaec4
ur <dorosi150@gmail.com>
2025-05-14 18:01:29 +0900

    refactor : CommonAdvice 예외 처리 수정
    NotFoundException, BadReqeustException 등 -> CommonHttpException으로 처리

--- src/main/java/com/nhnacademy/bookingservice/common/advice/CommonAdvice.java
+++ src/main/java/com/nhnacademy/bookingservice/common/advice/CommonAdvice.java
@@ -1,9 +1,6 @@
 package com.nhnacademy.bookingservice.common.advice;
 
-import com.nhnacademy.bookingservice.common.exception.BadRequestException;
-import com.nhnacademy.bookingservice.common.exception.ConflictException;
-import com.nhnacademy.bookingservice.common.exception.ForbiddenException;
-import com.nhnacademy.bookingservice.common.exception.NotFoundException;
+import com.nhnacademy.bookingservice.common.exception.*;
 import com.nhnacademy.bookingservice.controller.BookingController;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
@@ -39,49 +36,17 @@ public class CommonAdvice {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(commonErrorResponse);
     }
 
-    @ExceptionHandler(BadRequestException.class)
-    public ResponseEntity<CommonErrorResponse> badRequestExceptionHandler(BadRequestException e, HttpServletRequest request){
+    @ExceptionHandler(CommonHttpException.class)
+    public ResponseEntity<CommonErrorResponse> commonHttpExceptionHandler(CommonHttpException e, HttpServletRequest request){
         CommonErrorResponse errorResponse = new CommonErrorResponse(
                 e.getMessage(),
-                HttpStatus.BAD_REQUEST.value(),
+                e.getStatusCode(),
                 request.getRequestURI()
         );
 
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+        return ResponseEntity.status(e.getStatusCode()).body(errorResponse);
     }
 
-    @ExceptionHandler(ForbiddenException.class)
-    public ResponseEntity<CommonErrorResponse> forbiddenExceptionHandler(ForbiddenException e, HttpServletRequest request){
-        CommonErrorResponse errorResponse = new CommonErrorResponse(
-                e.getMessage(),
-                HttpStatus.FORBIDDEN.value(),
-                request.getRequestURI()
-        );
-
-        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(errorResponse);
-    }
-
-    @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<CommonErrorResponse> notFoundExceptionHandler(NotFoundException e, HttpServletRequest request){
-        CommonErrorResponse errorResponse = new CommonErrorResponse(
-                e.getMessage(),
-                HttpStatus.NOT_FOUND.value(),
-                request.getRequestURI()
-        );
-
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
-    }
-
-    @ExceptionHandler(ConflictException.class)
-    public ResponseEntity<CommonErrorResponse> conflictExceptionHandler(ConflictException e, HttpServletRequest request){
-        CommonErrorResponse errorResponse = new CommonErrorResponse(
-                e.getMessage(),
-                HttpStatus.CONFLICT.value(),
-                request.getRequestURI()
-        );
-
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(errorResponse);
-    }
 
     @ExceptionHandler(Throwable.class)
     public ResponseEntity<CommonErrorResponse> exceptionHandler(Throwable e, HttpServletRequest request){
