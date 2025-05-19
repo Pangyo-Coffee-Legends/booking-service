9015d884a6fa7fbba0b166bb47167edfbc6f4c78
LEMON4DE <111743489+LEMON4DE@users.noreply.github.com>
2025-05-19 16:59:45 +0900

    feature: Front에서 입력된 예약코드를 검증 (Resolves Pangyo-Coffee-Legends/backlog#111)

--- src/main/java/com/nhnacademy/bookingservice/common/exception/booking/BookingInfoDoesNotMatchException.java
+++ src/main/java/com/nhnacademy/bookingservice/common/exception/booking/BookingInfoDoesNotMatchException.java
@@ -1,9 +1,9 @@
 package com.nhnacademy.bookingservice.common.exception.booking;
 
-import com.nhnacademy.bookingservice.common.exception.BadRequestException;
+import com.nhnacademy.bookingservice.common.exception.NotFoundException;
 
 @SuppressWarnings("java:S110")
-public class BookingInfoDoesNotMatchException extends BadRequestException {
+public class BookingInfoDoesNotMatchException extends NotFoundException {
 
     public BookingInfoDoesNotMatchException() {
         super("예약정보가 일치하지 않습니다.");
