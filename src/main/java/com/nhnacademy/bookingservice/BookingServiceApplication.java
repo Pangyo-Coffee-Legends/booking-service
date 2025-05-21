854e4207074148bb0c73c41dfc06102da3a2f919
ur <dorosi150@gmail.com>
2025-05-21 11:09:31 +0900

    chore : 중앙 로그 서버 설정

--- src/main/java/com/nhnacademy/bookingservice/BookingServiceApplication.java
+++ src/main/java/com/nhnacademy/bookingservice/BookingServiceApplication.java
@@ -3,11 +3,13 @@ package com.nhnacademy.bookingservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableAsync
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 public class BookingServiceApplication {
 
     public static void main(String[] args) {
