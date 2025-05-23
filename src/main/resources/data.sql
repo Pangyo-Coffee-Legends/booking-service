e540f9e729f7efefe929effbc84fe11ffd7f6bf7
ur <dorosi150@gmail.com>
2025-05-23 15:12:24 +0900

    fix : Bookings DDL 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,31 +1,60 @@
-DROP TABLE IF EXISTS booking_changes;
-CREATE TABLE booking_changes (
-                                 change_no bigint NOT NULL COMMENT '특이사항 번호, autoincrement',
-                                 change_name varchar(10) NOT NULL COMMENT '특이사항 이름',
-
-                                 CONSTRAINT pk_change PRIMARY KEY (change_no)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-INSERT INTO bookint_chages(chnage_no, change_name) VALUES (1, '연장'),
-                                                          (2, '종료'),
-                                                          (3, '취소'),
-                                                          (4, '변경');
-
-
 DROP TABLE IF EXISTS bookings;
+DROP TABLE IF EXISTS meeting_rooms;
+DROP TABLE IF EXISTS booking_changes;
+
+CREATE TABLE meeting_rooms (
+                               meeting_room_no bigint NOT NULL COMMENT '회의실 번호, autoincrement',
+                               meeting_room_name varchar(10) NOT NULL COMMENT '회의실 이름',
+                               meeting_room_capacity int NOT NULL COMMENT '수용 인원',
+                               PRIMARY KEY (meeting_room_no)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+CREATE TABLE booking_changes (
+                                 change_no bigint NOT NULL COMMENT '예약 특이사항 번호, autoincrement',
+                                 change_name varchar(10) NOT NULL COMMENT '특이사항 이름',
+                                 PRIMARY KEY (change_no)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
 CREATE TABLE bookings (
                           booking_no bigint NOT NULL COMMENT '예약 번호, autoincrement',
                           mb_no bigint NOT NULL COMMENT '예약자 번호',
-                          room_no bigint NOT NULL COMMENT '회의실 번호',
-                          change_no bigint NOT NULL COMMENT '특이사항',
+                          meeting_room_no bigint NOT NULL COMMENT '회의실 번호',
+                          change_no bigint DEFAULT NULL COMMENT '특이사항',
                           booking_code varchar(10) NOT NULL COMMENT '예약 코드',
-                          booking_date timestamp NOT NULL COMMENT '예약 사작일자',
                           attendee_count int NOT NULL COMMENT '예약 인원',
-                          finishes_at timestamp NULL COMMENT '예약 종료일자',
+                          booking_date timestamp NOT NULL COMMENT '예약 시작일자',
+                          finishes_at timestamp DEFAULT NULL COMMENT '예약 종료일자',
                           created_at timestamp NOT NULL COMMENT '예약 생성일자',
-
-                          CONSTRAINT  pk_bookings PRIMARY KEY (booking_no),
-                          CONSTRAINT qk_bookings UNIQUE (booking_code),
-                          CONSTRAINT fj_booking_member FOREIGN KEY (mb_no) REFERENCES members(mb_no),
-                          CONSTRAINT fk_bookings_room FOREIGN KEY (room_no) REFERENCES rooms(room_no),
-                          CONSTRAINT fk_booking_change FOREIGN KEY (change_no) REFERENCES booking_chages(change_no)
+                          PRIMARY KEY (booking_no),
+                          UNIQUE KEY booking_code (booking_code)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+ALTER TABLE meeting_rooms
+    MODIFY meeting_room_no bigint NOT NULL AUTO_INCREMENT COMMENT '회의실 번호, autoincrement';
+
+ALTER TABLE booking_changes
+    MODIFY change_no bigint NOT NULL AUTO_INCREMENT COMMENT '예약 특이사항 번호, autoincrement';
+
+ALTER TABLE bookings
+    MODIFY booking_no bigint NOT NULL AUTO_INCREMENT COMMENT '예약 번호, autoincrement';
+
+ALTER TABLE bookings
+    ADD CONSTRAINT fk_bookings_members FOREIGN KEY (mb_no) REFERENCES members(mb_no),
+	ADD CONSTRAINT fk_bookings_rooms FOREIGN KEY (meeting_room_no) REFERENCES meeting_rooms(meeting_room_no),
+	ADD CONSTRAINT fk_bookings_changes FOREIGN KEY (change_no) REFERENCES booking_changes(change_no);
+
+INSERT INTO booking_changes(change_name) VALUES
+                                             ('연장'),
+                                             ('종료'),
+                                             ('취소'),
+                                             ('변경');
+
+INSERT INTO meeting_rooms (meeting_room_name, meeting_room_capacity) VALUES
+                                                                         ('회의실A', 10),
+                                                                         ('회의실B', 20),
+                                                                         ('회의실C', 15),
+                                                                         ('회의실D', 8),
+                                                                         ('회의실E', 25);
