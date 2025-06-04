95729644685831912797443d7dfb0d05e4b8e1db
LEMON4DE <111743489+LEMON4DE@users.noreply.github.com>
2025-06-04 21:18:42 +0900

    feature: 회의 연장 및 종료 기능 수정 closes Pangyo-Coffee-Legends/backlog#179

--- src/main/java/com/nhnacademy/bookingservice/dto/DailyBookingResponse.java
+++ src/main/java/com/nhnacademy/bookingservice/dto/DailyBookingResponse.java
@@ -23,13 +23,16 @@ public class DailyBookingResponse {
 
     private LocalDateTime finishesAt;
 
+    private String bookingChangeName;
+
     @QueryProjection
-    public DailyBookingResponse(Long no, Long mbNo, Integer attendeeCount, LocalDateTime startsAt, LocalDateTime finishesAt) {
+    public DailyBookingResponse(Long no, Long mbNo, Integer attendeeCount, LocalDateTime startsAt, LocalDateTime finishesAt, String bookingChangeName) {
         this.no = no;
         this.mbNo = mbNo;
         this.attendeeCount = attendeeCount;
         this.startsAt = startsAt;
         this.finishesAt = finishesAt;
+        this.bookingChangeName = bookingChangeName;
     }
 
 }
